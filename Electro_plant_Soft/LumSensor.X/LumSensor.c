d72a9df844508bcca3c2199a7c60cba272850270
Alex Ngo <alngo@student.42.fr>
2017-04-07 15:02:54 +0200

    Wip code lumsensor

--- Electro_plant_Soft/LumSensor.X/LumSensor.c
+++ Electro_plant_Soft/LumSensor.X/LumSensor.c
@@ -11,6 +11,8 @@
  * LDR -> AN5/C1INA/C2INC/RTCC/RPB3/SCL2/RB3 [Pin 5]
  */
 
+u16		ldr_value;
+
 void	init()
 {
 	//SET INITIAL STATE FOR :
@@ -18,13 +20,16 @@ void	init()
     TRISBbits.TRISB3 = 1;	// SET RB3/AN5 TO INPUT
     PORTBbits.RB3 = 0;		// SET THE LINE TO 0 ?
 
-
     //	ADC
     AD1CON1bits.ON = 0;		// DISABLE ADC1 FOR CONFIGURATION
-    AD1PCFGbits.PCFG5 = 0;	// /!\ I HAVE NO IDEA WHAT I'M DOING SET TO ANALOG RB3
-    AD1CON1bits.ASAM = 1;	// AUTOMATIC SAMPLING
+    AD1PCFGbits.PCFG5 = 0;	// /!\ SET TO ANALOG RB3
+    //AD1CON1bits.ASAM = 0;	// AUTOMATIC SAMPLING
     AD1CON1bits.FORM = 0x0;	// SET DATA OUTPUT FORMAT TO INTEGER
     AD1CHSbits.CH0SB3 = 1;	// SELECT CHANEL B3
+	AD1CON1bits.SSRC = 0b111;	// SELECT SAMPLE CLOCK SOURCE TO INTERNAL (AUTO)
+	AD1CON2 = 0;			// ALL AD1CON2 to 0;
+	AD1CSSL = 0;			// Skip Scanner
+	AD1CON3bits.SAMC = 0b11111;	// SET 31 TAD for SAMPLING
 
     //	TIMER
     T1CONbits.ON = 0;		// DISABLE TIMER FOR CONFIGURATION
@@ -46,12 +51,22 @@ void	init_interrupt()
     __asm ("ei");
 }
 
+void	__ISR(12, IPL1AUTO)		T3_Interrupt(void)
+{
+	AD1CON1bits.ON = 1;
+	AD1CON1CLR = 0x0002;	// CLEAR SAMP AND START THE CONVERSION PROCESS
+	while (!(AD1CON1 & 0x0001)); // WHILE PROCESS IS RUNNING
+	ldr_value = ADC1BUF0;
+	AD1CON1bits.ON = 0;
+	IFS0bits.T1IF = 0;
+}
+
 int	main()
 {
     init();
     while(1)
     {
-	WDTCONbits.WDTCLR = 0x01;
+		WDTCONbits.WDTCLR = 0x01;
     }
     return (1);
 }
