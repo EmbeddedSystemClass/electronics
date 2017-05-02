65c6f094976418ad3a04a655177cdad59d1aee74
Mathilde Leroi <mathilde.leroi@gmail.com>
2017-05-02 18:58:41 +0200

    Add moisture code without calibration

---
+++ Parts/Complete_code/MLeroi_POC_levelSensor.X/Soil_sensor.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   Soil_sensor.h
+ * Author: bocal
+ *
+ * Created on April 13, 2017, 11:34 AM
+ */
+
+#ifndef SOIL_SENSOR_H
+#define	SOIL_SENSOR_H
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+
+typedef char s8;
+typedef short s16;
+typedef int  s32;
+
+#endif	/* SOIL_SENSOR_H */
+
---
+++ Parts/Complete_code/MLeroi_POC_levelSensor.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=MLeroi_POC_levelSensor.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MLeroi_POC_levelSensor.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=mleroipoclevelsensor.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/mleroipoclevelsensor.x.tar
