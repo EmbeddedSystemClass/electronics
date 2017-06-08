e22a7e9293d466884d6695a8d7350cf54df007f2
Alx <alngo@student.42.fr>
2017-06-08 18:02:05 +0200

    Create light.h

--- E_O_E.X/light.h
+++ E_O_E.X/light.h
@@ -2,9 +2,12 @@
 #define LIGHT_H
 
 #define LIGHT_PIN 0
+#define THRESHOLD 100
 
 void    	check_light();
-uint32_t	get_light(void);
 uint8_t		uchar_to_hex(uint8_t val);
+void      init_auto_adc(void);
+void      init_manual_adc(void);
+uint32_t	get_light_manual(void)
 
 #endif
