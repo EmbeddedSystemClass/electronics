c1949dc278105dbc82ac383c61ba99c77176e32e
morgane <malbanes@student.42.fr>
2017-07-21 21:04:26 +0200

    a ajouter au pi

--- final_project/gnuplot_auto.c
+++ final_project/gnuplot_auto.c
@@ -1,20 +1,3 @@
-//dans le script
-//definir des type de ligne:
-//	set style line 10 linetype 1 linecolor rgb "" linewidth 1
-// a l'appel :  plot "name" linestyle 1
-//liste code couleur:
-//saumon : fa8072
-//orange : f4a460
-//rouge : ff0000
-//bleu : 4169e1
-//bleu fade : 6a5acd
-//vert : 2e8b57
-//violet : 800080
-//gris : 778899
-//or : ffd700
-//noir : 000000
-//blanc : ffffff
-
 //Dans le code du pi
 
 #include <stdlib.h>
@@ -26,13 +9,15 @@ int	main(void)
 	FILE	*fichier = NULL;
 	const char	*str = "10";
 	
-	if ((fichier = fopen("data.dat", "a+")) == NULL)
+	//if (mesures = 48)
+	//	system("rm data.dat");
+	if ((fichier = fopen("data.dat", "a+")) == NULL) //a+ = ecriture/lecture en partant de la fin du fichier et le créé si inexistant
 		return (-1);
-	fprintf(fichier,"%s\t", str);
+	fprintf(fichier,"%s\t", str); //comme printf
 	fclose(fichier);
 // fin ecriture
 	//system ("echo $data >> data.dat");
-	system("sh ~/Bureau/plot.sh"); // generation auto graphique gnuplot
+	system("sh ~/Bureau/plot.sh"); // generation auto graphique gnuplot !! changer les path ici et dans le script
 	return (0);
 }
 
